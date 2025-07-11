ed44e9919016c4f83847f88189bb37bd6c141956
renatorv <renatorv@gmail.com>
2025-07-10 22:39:18 -0300

    Aula 2 => 1 hora

--- main.py
+++ main.py
@@ -1,26 +1,69 @@
-from fastapi import FastAPI
+from datetime import datetime
+
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
+from starlette.status import HTTP_404_NOT_FOUND
 
 app = FastAPI()
 
 
 # Modelos / Classes para entrada e saída de dados do backend, usa o pydantic - SCHEMAS
 # Model para entrada e saída de dados do banco de dados - MODEL
+# SCHEMAS: entrada e saída de dados
 
 
 db_stores = []
 
-class Store(BaseModel):
-    name: str = Field(min_length=3, max_length=20)
+
+class StoreBase(BaseModel):
+    name: str = Field(min_length=4, max_length=20)
+    location: str | None = None
+
+class Store(StoreBase):
+    id: int
+
+class DbStore(Store):
+    create_at: datetime = datetime.now()
+
+class CreateStore(StoreBase):
+    pass
+
+class PatchStore(BaseModel):
+    name: str | None = Field(default=None, min_length=4, max_length=20)
     location: str | None = None
 
 
 @app.post("/stores", response_model=Store)
-def create_store(store: Store):
-    db_stores.append(store)
-    return store
+def create_store(store: CreateStore):
+    db_store = DbStore(id=len(db_stores), **store.model_dump())
+    db_stores.append(db_store)
+    return db_store
 
 
 @app.get("/stores", response_model=list[Store])
 def list_stores():
     return db_stores
+
+
+@app.get("/stores/{store_id}", response_model=Store)
+def get_store(store_id: int):
+    try:
+        return db_stores[store_id]
+    except IndexError:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Store not found.")
+
+
+# put = altera o objeto completo
+@app.put("/stores/{store_id}", response_model=Store)
+def update_store(store_id: int, store: CreateStore):
+    try:
+        db_store = DbStore(id=store_id, **store.model_dump())
+        db_stores[store_id] = db_store
+        return db_store
+    except IndexError:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Store not found.")
+
+# patch = atualiza somente os dados que foram passados
+@app.patch("/stores/{store_id}", response_model=Store)
+def patch_store(store_id: int, store: CreateStore):
+    pass
